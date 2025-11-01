648db40e9dc338963e015d278d0cb5d9823e2746
chan8632 <duscks8632@gmail.com>
2025-11-02 08:43:01 +0900

    Refactor: NumberList 검색 기능 로직 수정
    ### 본문 (선택 사항)
    
    * useState (matchingData)를 추가하여 필터링된 목록을 상태로 관리합니다.
    * useEffect를 사용하여 searchData 또는 phoneBook이 변경될 때만 검색 로직이 실행되도록 수정합니다.
    * 항상 전체 phoneBook을 렌더링하는 대신, 필터링된 matchingData 상태를 렌더링하도록 변경합니다.

--- src/components/NumberList.jsx
+++ src/components/NumberList.jsx
@@ -1,21 +1,26 @@
+import { useEffect, useState } from "react";
 import usephoneBookStore from "../stores/usephoneBookStores";
 import useSearchStore from "./../stores/useSearchStore";
 
 const NumberList = () => {
   const { phoneBook } = usephoneBookStore();
   const { searchData } = useSearchStore();
+  const [matchingData, setMatchingData] = useState(phoneBook);
   const matchContactAndSearch = () => {
     const oriPhoneBook = [...phoneBook];
-    if (searchData === "") return oriPhoneBook;
-    const matchingData = oriPhoneBook.filter((contact) =>
+    if (searchData === "") return setMatchingData(oriPhoneBook);
+    const matching = oriPhoneBook.filter((contact) =>
       contact.name.match(searchData)
     );
+    setMatchingData(matching);
   };
-  matchContactAndSearch();
-  
+  useEffect(() => {
+    matchContactAndSearch();
+  }, [searchData, phoneBook]);
+
   return (
     <div>
-      {phoneBook.map((info) => {
+      {matchingData.map((info) => {
         return (
           <div key={info.id}>
             <p>{info.name}</p>
